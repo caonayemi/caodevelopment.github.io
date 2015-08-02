4958526e4fb0d93c858f0f707d1c6ed24f558fe1
caonayemi <caonayemi@gmail.com>
2015-08-02 11:15:41 -0500

    Remove erb commands from main controller (these are used in Sinatra, not Rails)

--- CAODevelopment/app/controllers/main_controller.rb
+++ CAODevelopment/app/controllers/main_controller.rb
@@ -1,13 +1,10 @@
 class MainController < ApplicationController
   def index
-    erb :"main/index"
   end
 
   def about_tine
-    erb :"main/about_tine"
   end
 
   def contact
-    erb :"main/contact"
   end
 end
